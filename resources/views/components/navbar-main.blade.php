3f2984b49699c8d1e2de9bac377bb35b6993e4ef
Markus Dito <markusdito@gmail.com>
2024-10-29 00:11:26 +0700

    fix hamburger menu position

--- resources/views/components/navbar-main.blade.php
+++ resources/views/components/navbar-main.blade.php
@@ -53,10 +53,12 @@
         </li>
     </ul>
     @guest
+        <div class="absolute">
+            @include('auth.login')
+        </div>
         <a href=""
            class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold rounded-xl transition duration-200">Sign
             In</a>
-        @include('auth.login')
         <a href=""
            class="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200">Sign
             Up</a>
