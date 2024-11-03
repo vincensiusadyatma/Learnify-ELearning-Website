92c995d61fd10f52bc863253c0988bccadc2d7bd
Markus Dito <markusdito@gmail.com>
2024-11-03 10:02:44 +0700

    remake dashboard

--- resources/views/Components/sidebar/list-item.blade.php
+++ resources/views/Components/sidebar/list-item.blade.php
@@ -1,8 +1,7 @@
 @props(['title'])
 
 <li>
-    <a href="#"
-       class="flex items-center p-1.5 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+    <a href="#" class="flex items-center py-1.5 px-3 text-white text-opacity-80 font-light">
         <svg
             class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="w-svw">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,9 +7,9 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    @vite(['resources/css/app.css'])
 </head>
-<body>
+<body class="bg-[#EAECF3] ">
 <x-dashboard.main></x-dashboard.main>
 </body>
 </html>
