16609e11698c514b38046fe82a124bffdbdd409e
vincensiusadyatma <adyatmav@gmail.com>
2024-11-04 11:01:53 +0700

    feat : add get user data model to dashboard

--- app/Http/Controllers/Core/DashboardController.php
+++ app/Http/Controllers/Core/DashboardController.php
@@ -1,12 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\Core;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function showDashboard(){
-        return view('dashboard.main');
+        $user = Auth::user();
+        dd($user);
+        
+        return view('dashboard.main',[
+            'user' => $user
+        ]);
     }
 }
