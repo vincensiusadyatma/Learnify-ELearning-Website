bd984fce3be690e7982ceb3f977342656e7c21c1
NateNats <nicolaussagraha14@gmail.com>
2024-11-25 19:53:25 +0700

    change directory for course images

--- resources/views/core/layouts/card.blade.php
+++ resources/views/core/layouts/card.blade.php
@@ -1,7 +1,7 @@
 @props(['title', 'description', 'link', 'icons'])
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="">
-    <img class="rounded-t-lg" src="{{ asset('storage/icons/' . $icons) }}" alt="{{$icons}}">
+    <img class="rounded-t-lg" src="{{ asset('./img/assets/course/' . $icons) }}" alt="{{$icons}}">
     </a>
     <div class="p-5">
         <a href="#">
@@ -16,12 +16,3 @@
         </a>
     </div>
 </div>
-
-
-<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="{{ asset('storage/icons/' . $icons) }}" alt="">
-    <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    </div>
-</a>
