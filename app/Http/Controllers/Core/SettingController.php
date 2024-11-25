2e4299a84e3274a80b3c2cec457d9b3ad6904058
Markus Dito <markusdito@gmail.com>
2024-11-25 17:38:16 +0700

    initialize profile bade

--- app/Http/Controllers/Core/SettingController.php
+++ app/Http/Controllers/Core/SettingController.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers\Core;
 
-class SettingController
-{
+use App\Http\Controllers\Controller;
 
+class SettingController extends Controller
+{
+    public function showSetting()
+    {
+        return view('core.setting');
+    }
+
+    public function showProfile()
+    {
+        return view('core.profile');
+    }
 }
