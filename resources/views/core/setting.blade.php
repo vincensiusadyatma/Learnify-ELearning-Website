fa10182f16354e53bb9dcc2778f78b80e56546eb
Markus Dito <markusdito@gmail.com>
2024-11-27 23:39:29 +0700

    account settings done

--- resources/views/core/setting.blade.php
+++ resources/views/core/setting.blade.php
@@ -1,14 +1,14 @@
 @extends('core.layouts.index')
 
 @section('core-content')
-    <div class="flex flex-col w-full h-[900px]">
+    <div class="flex flex-col w-full h-[900px] bg-white-theme/[0.67]">
         {{-- title --}}
-        <div class="flex justify-start  items-center pl-4 w-full py-4 bg-white-theme/[0.67]">
+        <div class="flex justify-start  items-center pl-4 w-full py-4">
             <p>Settings</p>
         </div>
 
         {{-- Navigation --}}
-        <div class="flex w-full gap-52 py-6 items-center justify-center border border-blue-500">
+        <div class="flex w-full gap-52 py-6 items-center justify-center">
             <a class="">
                 Account
             </a>
@@ -18,26 +18,26 @@
         </div>
 
         {{-- Account Form --}}
-        <div>
-            <div>
-                <label for="username-setting">Username</label>
-                <form action="" id="username-setting"></form>
+        <div class="flex flex-col h-full w-full px-24">
+            <div class="flex flex-col h-[150px] gap-7">
+                <label for="username-setting" class="text-xl font-bold">Username</label>
+                <input class="h-10 bg-transparent border border-gray-300" id="username-setting"/>
             </div>
-            <div>
-                <label for="username-setting">Username</label>
-                <form action="" id="username-setting"></form>
+            <div class="flex flex-col h-[150px] gap-7">
+                <label for="email-setting" class="text-xl font-bold">Email</label>
+                <input class="h-10 bg-transparent border border-gray-300" id="email-setting"/>
             </div>
-            <div>
-                <label for="username-setting">Username</label>
-                <form action="" id="username-setting"></form>
+            <div class="flex flex-col h-[150px] gap-7">
+                <label for="fullname-setting" class="text-2xl font-bold">Nama Lengkap</label>
+                <input class="h-10 bg-transparent border border-gray-300" id="fullname-setting"/>
             </div>
-            <div>
-                <label for="username-setting">Username</label>
-                <form action="" id="username-setting"></form>
+            <div class="flex flex-col h-[150px] gap-7">
+                <label for="phone-setting" class="text-2xl font-bold">No HP</label>
+                <input class="h-10 bg-transparent border border-gray-300" id="phone-setting"/>
             </div>
-            <div class="w-full h-full flex items-center justify-center">
-                <button class="flex items-center justify-center w-[250px] h-10 bg-green-500">
-                    <p>Save</p>
+            <div class="flex items-center justify-center">
+                <button class="flex items-center justify-center w-[250px] h-10 bg-[#5271FF] rounded-md">
+                    <p class="text-white font-bold">Save</p>
                 </button>
             </div>
         </div>
