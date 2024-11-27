18101edc38caaf0fdd585b05c3d4ce71a02e37dd
Markus Dito <markusdito@gmail.com>
2024-11-27 23:55:27 +0700

    add js to settings

--- resources/views/core/setting.blade.php
+++ resources/views/core/setting.blade.php
@@ -9,16 +9,16 @@
 
         {{-- Navigation --}}
         <div class="flex w-full gap-52 py-6 items-center justify-center">
-            <a class="">
+            <button id="account-btn" class="">
                 Account
-            </a>
-            <a class="">
+            </button>
+            <button id="pw-btn" class="">
                 Password
-            </a>
+            </button>
         </div>
 
         {{-- Account Form --}}
-        <div class="flex flex-col h-full w-full px-24">
+        <div id="account-form" class="flex flex-col w-full px-24">
             <div class="flex flex-col h-[150px] gap-7">
                 <label for="username-setting" class="text-xl font-bold">Username</label>
                 <input class="h-10 bg-transparent border border-gray-300" id="username-setting"/>
@@ -37,9 +37,58 @@
             </div>
             <div class="flex items-center justify-center">
                 <button class="flex items-center justify-center w-[250px] h-10 bg-[#5271FF] rounded-md">
-                    <p class="text-white font-bold">Save</p>
+                    <p id="account-save" class="text-white font-bold">Save</p>
                 </button>
             </div>
         </div>
+
+        {{-- Password Form--}}
+        <div id="pw-form" class="hidden flex-col w-full px-24">
+            <div class="flex flex-col h-[150px] gap-7">
+                <label for="pw-now-setting" class="text-xl font-bold">Current Password</label>
+                <input class="h-10 bg-transparent border border-gray-300" id="pw-now-setting"/>
+            </div>
+            <div class="flex flex-col h-[150px] gap-7">
+                <label for="pw-now-setting" class="text-xl font-bold">New Password</label>
+                <input class="h-10 bg-transparent border border-gray-300" id="pw-new-setting"/>
+            </div>
+            <div class="flex flex-col h-[150px] gap-7">
+                <label for="pw-conf-setting" class="text-2xl font-bold">Confirm New Password</label>
+                <input class="h-10 bg-transparent border border-gray-300" id="pw-conf-setting"/>
+            </div>
+            <div class="flex items-center justify-center">
+                <button class="flex items-center justify-center w-[250px] h-10 bg-[#5271FF] rounded-md">
+                    <p id="account-save" class="text-white font-bold">Save</p>
+                </button>
+            </div>
+        </div>
+
+
     </div>
+
 @endsection
+
+@push('additional-scripts')
+    <script>
+        const accountForm = document.querySelector('#account-form')
+        const pwForm = document.querySelector('#pw-form')
+        const accountBtn = document.querySelector('#account-btn')
+        const pwBtn = document.querySelector('#pw-btn')
+
+        accountBtn.addEventListener('click', () => {
+            accountForm.classList.add('flex')
+            pwForm.classList.add('hidden')
+
+            accountForm.classList.remove('hidden')
+            pwForm.classList.remove('flex')
+        })
+
+        pwBtn.addEventListener('click', () => {
+            accountForm.classList.add('hidden')
+            pwForm.classList.add('flex')
+
+            accountForm.classList.remove('flex')
+            pwForm.classList.remove('hidden')
+        })
+    </script>
+@endpush
